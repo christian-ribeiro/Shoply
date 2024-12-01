87eac213249a9e41931c74abea23ece75bbbd3c3
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-12-01 17:36:55 -0300

    feat(api): Add Swagger

---
+++ src/Shoply.Api/Extensions/SwaggerExtension.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Shoply.Api.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
+            const string BearerScheme = "Bearer";
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Shoply API",
+                    Description = "API de demonstração de conceito",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Christian Ribeiro",
+                        Url = new Uri("https://github.com/christian-ribeiro")
+                    }
+                });
+
+                c.AddSecurityDefinition(BearerScheme, new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = BearerScheme
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = BearerScheme
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+
+                c.TagActionsBy(api =>
+                {
+                    var actionDescriptor = api.ActionDescriptor as ControllerActionDescriptor;
+                    return [actionDescriptor!.ControllerName];
+                });
+            });
+
+            return services;
+        }
+        public static WebApplication ApplySwagger(this WebApplication app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shoply API v1");
+                c.DocumentTitle = "Shoply API Documentation";
+                c.EnableFilter();
+                c.DocExpansion(DocExpansion.None);
+                c.DisplayRequestDuration();
+            });
+            return app;
+        }
+    }
+}
--- src/Shoply.Api/Program.cs
+++ src/Shoply.Api/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureContext();
 builder.Services.ConfigureCors();
+builder.Services.ConfigureSwagger();
 builder.Services.ConfigureController();
 builder.Services.ConfigureMapper();
 
@@ -15,6 +16,7 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 app.UseStaticFiles();
 app.ApplyCors();
+app.ApplySwagger();
 app.ApplyController();
 
 app.Run();
