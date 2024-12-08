6284484824db7e3e348bb998508617c8921b7346
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-12-08 16:55:41 -0300

    chore(remove duplicated interface)

--- src/Shoply.Domain/Interface/Service/Module/Registration/Customer/CustomerAddress/ICustomerAddressService.cs
+++ src/Shoply.Domain/Interface/Service/Module/Registration/Customer/CustomerAddress/ICustomerAddressService.cs
@@ -4,7 +4,4 @@ using Shoply.Domain.Interface.Service.Base;
 
 namespace Shoply.Domain.Interface.Service.Module.Registration;
 
-public interface ICustomerAddressService : IBaseService<InputCreateCustomerAddress, InputUpdateCustomerAddress, InputIdentifierCustomerAddress, OutputCustomerAddress, InputIdentityUpdateCustomerAddress, InputIdentityDeleteCustomerAddress>
-{
-    Task<BaseResult<List<OutputCustomerAddress?>>> GetDynamic(string[] fields);
-}
+public interface ICustomerAddressService : IBaseService<InputCreateCustomerAddress, InputUpdateCustomerAddress, InputIdentifierCustomerAddress, OutputCustomerAddress, InputIdentityUpdateCustomerAddress, InputIdentityDeleteCustomerAddress> { }
